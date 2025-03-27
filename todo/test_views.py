ab99f3c1149e42d6b00d07ef78361c7f67e88946
mohammad-tahmasebi-ci <mohammad.tahmasebi@students.codeinstitute.net>
2025-03-27 20:51:08 +0000

    install coverage module and get a report of percentage code covered by tests

--- todo/test_views.py
+++ todo/test_views.py
@@ -38,4 +38,8 @@ class TestViews(TestCase):
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
 
-        
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo List')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, "Updated Name")
