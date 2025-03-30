60bd4ecdcd9132711303a0d5e79a46ec6fc25191
mohammad-tahmasebi-ci <mohammad.tahmasebi@students.codeinstitute.net>
2025-03-30 13:04:16 +0100

    modify typo in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
@@ -85,7 +86,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #    }
 #}
 
-DATABASE = {
+DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
